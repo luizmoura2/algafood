46cb5b36e23761e43f047ecfd16f488712f2fd49
Luiz Moura <luizmoura2@hotmail.com>
2020-07-23 12:28:40 -0300

    14.23. Implementing the inclusion of objects in the Amazon S3 bucket

--- src/main/java/com/algaworks/algafood/infraestrutura/service/S3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infraestrutura/service/S3FotoStorageService.java
@@ -1,17 +1,16 @@
 package com.algaworks.algafood.infraestrutura.service;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.util.FileCopyUtils;
 
 import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.service.FotoStorageService;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 //@Service
 public class S3FotoStorageService implements FotoStorageService {
@@ -22,9 +21,29 @@ public class S3FotoStorageService implements FotoStorageService {
 	@Autowired
 	private AmazonS3 amazonS3;
 	
+	@Autowired
+	private StorageProperties storageProperties;
+	
 	@Override
 	public void armazenar(NovaFoto novaFoto) {
-		//
+		
+		try {
+			
+			String caminhaArquivo = getCaminhoArquivo(novaFoto.getNomeAquivo());
+			var objMetadata = new ObjectMetadata();
+			objMetadata.getContentType(novaFoto.get);
+			var putObjectRequest = new PutObjectRequest(
+					storageProperties.getS3().getBucket(),
+					caminhaArquivo,
+					novaFoto.getInputStream(),
+					objMetadata).withCannedAcl(CannedAccessControlList.PublicRead);
+			
+			amazonS3.putObject(putObjectRequest);
+		
+		}catch(Exception e) {
+			throw new StorageException("Não foi possível enviar o arquivo para amazon S3", e );
+		}
+		
 	}
 	
 	@Override
@@ -38,6 +57,11 @@ public class S3FotoStorageService implements FotoStorageService {
 		//
 	}
 
+	private String getCaminhoArquivo(String nomeArquivo) {
+		return String.format("%s/%s", storageProperties.getS3().getDiretorioFotos(), nomeArquivo);
+		
+	}
+	
 	private Path getArquivoPath(String nomeArquivo) {
 		return null;
 		//
